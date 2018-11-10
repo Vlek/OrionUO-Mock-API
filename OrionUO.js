d3365970838642349075e5e424ba9fbdfa7eabb8
Vlek <dmm545@drexel.edu>
2018-11-09 23:31:59 -0500

    Added some more methods. Thought over some interesting features.
    Potentially, what I would like to do is create a mocked out
    game session with underlying variables that can be set. That
    would allow people to test their scripts without necessarily
    having to run them in game.
    
    Also, I probably need to turn these objects into functions
    with constructors. I'm afraid however that the methods
    then wouldn't be visible via static analysis. I'll do
    some tests to see whether that's the case.

--- OrionUO.js
+++ OrionUO.js
@@ -1,9 +1,23 @@
 // Mock OrionUO Scripting API
 // by Vlek
 
+// TODO: Turn all of these into functions with constructors!!
+// TODO: Verify, if I do that, that static analysis will be able
+//      to see all of the methods that the object would have
+
+var GameObject = {
+    /**
+     * @return {string}
+     */
+    Serial: function() {
+        return "0x00003";
+    }
+};
+
 var Orion = {
+    // TODO: for all of the find functions, do consistent parameter verification
     debug: false,
-    CharPrint: function(serial, color, text) {
+    CharPrint: function (serial, color, text) {
         if (!serial || !color || !text) {
             throw "Not enough parameters!";
         }
@@ -13,7 +27,68 @@ var Orion = {
                 "with '" + text + "' in color " + color);
         }
     },
-    Print: function(color, text) {
+    ClearJournal: function() {
+        // TODO: clear the mocked journal object
+        if (this.debug) {
+            console.log("Clearing journal!");
+        }
+    },
+    /**
+     * @return {string}
+     */
+    FindFriend: function(flags, distance) {
+        return "0x000006";
+    },
+    FindObject: function(serial) {
+        if (!serial) {
+            throw "Not enough parameters!";
+        }
+        // TODO: Fix this when I fix the GameObject
+        return GameObject;
+    },
+    FindType: function(graphic, color, container, flags, distance, notoriety, recurse) {
+        if (!graphic) {
+            throw "Not enough parameters!";
+        }
+
+        if (this.debug) {
+            console.log("Trying to find graphic: " + graphic);
+        }
+    },
+    /**
+     * @return {boolean}
+     */
+    InJournal: function(pattern, flags, serial, color, startTime, endTime) {
+        if (!pattern) {
+            throw "Not enough parameters!";
+        }
+        // TODO: Mock out a journal object
+        return false;
+    },
+    MoveItem: function(serial, count, container, x, y, z) {
+        // MoveItem technically doesn't have any required parameters.
+        // It will just fail quietly.
+
+        if (this.debug) {
+            console.log("Moving item!");
+        }
+    },
+    /**
+     * @return {number}
+     */
+    Now: function() {
+        return Date.now();
+    },
+    ObjAtLayer: function(layerName, serial) {
+        if (!layerName) {
+            throw "Not enough parameters!";
+        }
+
+        // TODO: When I fix GameObject with a constructor,
+        // make this a function call with new
+        return GameObject;
+    },
+    Print: function (color, text) {
         if (!text) {
             text = color;
             color = -1;
@@ -23,17 +98,17 @@ var Orion = {
             console.log("Printing '" + text + "' with color " + color)
         }
     },
-    SaveConfig: function() {
+    SaveConfig: function () {
         if (this.debug) {
             console.log("Config saved!");
         }
     },
-    Say: function(text) {
+    Say: function (text) {
         if (this.debug) {
             console.log("Saying: " + text);
         }
     },
-    SetLight: function(state, value) {
+    SetLight: function (state, value) {
         // TODO: Mock out the light levels, see what values can be
         // in place and also have a state probably in the Orion
         // namespace that tracks the state of the light level.
@@ -44,8 +119,78 @@ var Orion = {
         if (this.debug) {
             console.log("Setting light level");
         }
+    },
+    UseSkill: function(skillname, target) {
+        if (!skillname) {
+            throw "Not enough parameters!";
+        }
+
+        // TODO: Verify that skillname is an actual skill
+        if (this.debug) {
+            console.log("Using skill: " + skillname);
+        }
+    },
+    Wait: function(timeout) {
+        if (this.debug) {
+            console.log("Waiting " + timeout + " ms");
+        }
     }
 
+};
+
+var Player = {
+    // TODO: Create player constants that let people change their internal
+    // values to mock out a "session" with a character so that they can try
+    // their scripts out in more realistic ways. So Hidden will return true
+    // or false whether Player._isHidden is true or false, etc
+    /**
+     * @return {boolean}
+     */
+    Dead: function() {
+        return false;
+    },
+    /**
+     * @return {boolean}
+     */
+    Hidden: function() {
+        return false;
+    },
+    /**
+     * @return {number}
+     */
+    Mana: function() {
+        return 100;
+    },
+    /**
+     * @return {number}
+     */
+    MaxMana: function() {
+        return 100;
+    },
+    /**
+     * @return {string}
+     */
+    Serial: function() {
+        return "0x000001";
+    },
+    /**
+     * @return {number}
+     */
+    X: function() {
+        return 1;
+    },
+    /**
+     * @return {number}
+     */
+    Y: function() {
+        return 2;
+    },
+    /**
+     * @return {number}
+     */
+    Z: function() {
+        return 3;
+    }
 };
 
 Orion.Print("1");
